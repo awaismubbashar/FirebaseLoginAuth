87297d617cfce67110a70d8965b994bbd670d3d1
Awais Mubbashar <awaismubbasharCL>
2023-05-25 18:16:59 +0500

    Added functionality of LoginWithGoogle on SignIn screen.

--- app/src/main/java/com/example/firebaseloginauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseloginauth/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.firebaseloginauth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebaseloginauth.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +21,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val email = intent.extras?.getString("email")
+        val name = intent.extras?.getString("name")
+
+        Toast.makeText(this, "$name, $email ", Toast.LENGTH_SHORT).show()
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
+            googleSignInClient.signOut()
 
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/firebaseloginauth/SignInActivity.kt
+++ app/src/main/java/com/example/firebaseloginauth/SignInActivity.kt
@@ -1,24 +1,44 @@
 package com.example.firebaseloginauth
 
+import android.app.Activity
 import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.firebaseloginauth.databinding.ActivitySignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivitySignInBinding.inflate(layoutInflater, )
+        binding = ActivitySignInBinding.inflate(layoutInflater,)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
@@ -27,6 +47,16 @@ class SignInActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             performLogin()
+        }
+
+        if (checkGooglePlayServices()) {
+            binding.loginWithGoogle.visibility = View.VISIBLE
+        } else {
+            binding.loginWithGoogle.visibility = View.GONE
+        }
+
+        binding.loginWithGoogle.setOnClickListener {
+            performLoginWithGoogle()
         }
     }
 
@@ -58,5 +88,56 @@ class SignInActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun performLoginWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this , MainActivity::class.java)
+                intent.putExtra("email" , account.email)
+                intent.putExtra("name" , account.displayName)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    private fun checkGooglePlayServices(): Boolean {
+        val availability = GoogleApiAvailability.getInstance()
+        val resultCode = availability.isGooglePlayServicesAvailable(this)
+        if (resultCode != ConnectionResult.SUCCESS) {
+//            val dialog = availability.getErrorDialog(this, resultCode, 0)
+//            dialog.show()
+            return false
+        }
+        return true
     }
 }
