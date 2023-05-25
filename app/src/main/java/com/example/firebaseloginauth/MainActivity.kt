d6f86cf7a1e2bf883b43c7b6f2c30660e7ff419c
Awais Mubbashar <awaismubbasharCL>
2023-05-25 14:54:41 +0500

    Done with firebase setup. Added signIn and SignUp authentication by using firebase

--- app/src/main/java/com/example/firebaseloginauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseloginauth/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.firebaseloginauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebaseloginauth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
